4ece335e55ce8dcfb084d23de5dd06f8e54bc60d
Satyam Dwivedi <dwivedisatyam167@gmail.com>
2025-07-18 15:04:49 +0530

    Delete 01practice.js

--- 01practice.js
+++
@@ -1,9 +0,0 @@
- 
- var names = ["Piyush" ,"Satyam"," Toshif " ,"sahil","tomKar"];
-
-function whosTurn(names) {
-    var numberofPeople = names.length;
-    var randomPerson = Math.floor(Math.random() * numberofPeople);
-    return names[randomPerson] + " your turn now ";
-}
-console.log(whosTurn(names));
